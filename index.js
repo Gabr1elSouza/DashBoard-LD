7488b08a67d2997ca03c0bd3d57f6dd3db91d155
Gabriel Souza <gablgdario03@gmail.com>
2023-06-26 22:39:24 -0300

    Merge branch 'main' of https://github.com/Gabr1elSouza/DashBoard-LD

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ closeBtn.addEventListener('click', ()=>{
     sideMenu.style.display= 'none';
 })
 
-//Change thme
+//Change theme
 themeToggler.addEventListener('click',()=>{
     document.body.classList.toggle("dark-theme-variables");
 
@@ -24,14 +24,14 @@ themeToggler.addEventListener('click',()=>{
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent= `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.shipping === 'Decline' ? 'danger' : 
-        order.shipping === 'pending' ? 'warning' : 'primary'}
-        ">${order.shipping}</td>
-        <td class="primary">Details</td>
-        `;
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Decline' ? 'danger' : 
+                        order.shipping === 'pending' ? 'warning' : 'primary'}
+                        ">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                        `;
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 
