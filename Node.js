9d358e5a6aa9dc1693d8956f9201a05a82dac5c0
Gabriel Souza <gablgdario03@gmail.com>
2023-07-12 14:42:19 -0300

    Add CSS and JS in server

--- Node.js
+++ Node.js
@@ -1,17 +1,19 @@
 const http = require('http')
 const fs = require('fs')
+const express =require('express')
+const app = express()
+const path = require('path')
 
 const port = 3000
 
-const server = http.createServer((req,res)=>{
-    fs.readFile('index.html', function(err, data){
-        res.writeHead(200, {'Content-type': 'text/html'})
-        res.write(data)
-        return res.end()
-    })
+const basePath = path.join(__dirname, 'templates')
 
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(`${basePath}/index.html`)
 })
 
-server.listen(port,()=>{
-    console.log(`Servidor rodando na porta: ${port}`)
+app.listen(port, ()=>{
+    console.log(`Serve working in port ${port}`)
 })
